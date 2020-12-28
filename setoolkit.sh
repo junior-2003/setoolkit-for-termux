2e45023538d66ff21af1eabd0e349cb4e5fcc6c4
junior-2003 <76687655+junior-2003@users.noreply.github.com>
2020-12-28 22:32:34 +0200

    Update setoolkit.sh

--- setoolkit.sh
+++ setoolkit.sh
@@ -1,7 +1,7 @@
 #!/data/data/com.termux/files/usr/bash
 echo "          **************************"
 echo
-echo "      subscribe   Tech Z India"
+echo "      subscribe  kiddo kyle 
 echo
 echo "          **************************"
 echo
